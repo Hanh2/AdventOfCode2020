9df9c9303b9d45a2cc41bfcbc22efc23592f3d06
Hanh2 <denhanh.h@gmail.com>
2020-12-03 06:44:24 +0100

    Add solution for 3b

---
+++ Day 3/day3b.py
@@ -0,0 +1,21 @@
+from day3a import get_map_as_list_of_strings
+from day3a import calc_nbr_trees_encountered
+
+if __name__ == '__main__':
+    # parameters
+    file_name = 'input.txt'
+    tree = '#'
+    slopes = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2]]
+
+    map_of_area = get_map_as_list_of_strings(file_name)
+    product = 1
+    for (right, down) in slopes:
+        nbr_trees = calc_nbr_trees_encountered(map_of_area, tree, right, down)
+        product *= nbr_trees
+    print(f'Product of the number of trees encountered' \
+          f' on each of the listed slopes: {product}')
