75223bcd89526ed5ed5921d8f6af7d5b2cfb6d86
Hanh2 <denhanh.h@gmail.com>
2020-12-06 21:01:56 +0100

    feat: add solution to 5a

---
+++ Day 5/day5a.py
@@ -0,0 +1,21 @@
+from utils.parser import get_input_as_list_of_strings
+
+
+def seat_string_to_id(seat_string: str) -> int:
+    binary_string = seat_string_to_binary_string(seat_string)
+    seat_id = int(binary_string, base=2)
+    return seat_id
+
+
+def seat_string_to_binary_string(seat_string: str) -> str:
+    binary_string = ''.join(
+        ['1' if (ch == 'B' or ch == 'R') else '0' for ch in seat_string]
+    )
+    return binary_string
+
+
+if __name__ == '__main__':
+    filename = 'input.txt'
+    boarding_passes = get_input_as_list_of_strings(filename)
+    seat_ids = map(seat_string_to_id, boarding_passes)
+    print(f'The highest seat id is {max(seat_ids)}.')
