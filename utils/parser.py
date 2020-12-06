cabc2266433ac129e0f7aa408940f240d14de2ce
Hanh2 <denhanh.h@gmail.com>
2020-12-06 21:31:26 +0100

    feat: allow input to be split by blank lines

--- utils/parser.py
+++ utils/parser.py
@@ -4,10 +4,12 @@ def get_input_as_list_of_ints(filename: str) -> list:
     return list_of_ints
 
 
-def get_input_as_list_of_strings(filename: str) -> list:
+def get_input_as_list_of_strings(
+        filename: str, separate_by_blank_lines: bool = False) -> list:
+    separator = '\n' if not separate_by_blank_lines else '\n\n'
     with open(filename, 'r') as f:
         # f.readlines() includes an unwanted '\n' at the end of each line
-        list_of_strings = f.read().split('\n')
+        list_of_strings = f.read().split(separator)
     return list_of_strings
 
 
